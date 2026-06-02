43e9f1ab69a11f96085cf51965fcdc79db524c0d
vaasthya <vaasthya@gmail.com>
2026-06-02 09:52:09 +0530

    Update vaasthya_tracker.py

--- vaasthya_tracker.py
+++ vaasthya_tracker.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-import matplotlib pyplot as plt
+import matplotlib.pyplot as plt
 
 # ==================================================
 # PAGE CONFIG
